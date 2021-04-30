cd9ac452aa9550a540e6bbb1e98ea34c6700c41b
Andrew Mikaeel <andrew.mikaeel@ontariotechu.net>
2021-04-30 15:01:31 -0400

    search screen template complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   navActive: boolean = false;
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    if(event.key === "Escape")
+      this.close();
+  }
 
 
   closeAnimation() {
